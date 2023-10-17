c145cc2f63feb882f9b3a075df684be39ba3967c
Tyler Cagle <tylerjcagle@gmail.com>
2023-10-17 10:53:08 -0400

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
@@ -6,11 +7,21 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preload" href="/assets/air.avif" as="image" />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-M8ML49PYNT" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-M8ML49PYNT');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
         <NextScript />
-        <Analytics mode={'production'} />
+        <Analytics mode={"production"} />
       </body>
     </Html>
   );
