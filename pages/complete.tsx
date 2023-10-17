c480dfb7ed6477038439441a510d99da4102474a
Tyler Cagle <tylerjcagle@gmail.com>
2023-10-17 09:34:55 -0400

    update kevy

--- pages/complete.tsx
+++ pages/complete.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import styles from "@/styles/main.module.scss";
 import { SEO } from "@/seo";
 import { Nav } from "@/components";
+import Balancer from "react-wrap-balancer";
 
 export default function Home() {
   const [countdown, setCountdown] = useState(10);
@@ -42,7 +43,7 @@ export default function Home() {
         <Nav />
         <div className="flex flex-col justify-center items-center gap-4 w-full h-full leading-tight text-center">
           <Image
-            src="/assets/kj.png"
+            src="/assets/kj.avif"
             alt="the man, the myth, the legend"
             width={300}
             height={300}
@@ -52,17 +53,27 @@ export default function Home() {
           />
           <h3 className="font-bold leading-tight">Thanks for your payment</h3>
           <div className="flex flex-col gap-2">
-            <h5>Your item will ship as soon as possible.</h5>
             <h5>
-              If you have any questions, please contact{" "}
-              <Link
-                href="https://twitter.com/sammcallister"
-                target="_blank"
-                className="underline"
-              >
-                @sammcallister
-              </Link>{" "}
-              on Twitter.
+              <Balancer>
+                A payment to{" "}
+                <Link
+                  href="https://embedcheckout.com"
+                  target="_blank"
+                  className="underline"
+                >
+                  embedcheckout.com
+                </Link>{" "}
+                will appear on your statement. Items will ship in 2–3 weeks. If
+                you have any questions about your order, please reach out to{" "}
+                <Link
+                  href="https://twitter.com/sammcallister"
+                  target="_blank"
+                  className="underline"
+                >
+                  @sammcallister
+                </Link>
+                .
+              </Balancer>
             </h5>
             <p className="mt-8 opacity-50">{note}</p>
           </div>
